efad645ac2dfde88a5a7759e94c5295ee470e32b
kimjangsoo <wallystudio@naver.com>
2017-03-20 02:38:49 +0000

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :followers, through: :follower_relations, source: :follower
   has_many :following_relations, foreign_key: "follower_id", class_name: "Follow"
   has_many :followings, through: :following_relations, source: :followed
+  mount_uploader :avatar, AvatarUploader
   
   def is_like?(post)
     Like.find_by(user_id: self.id, post_id: post.id).present?
